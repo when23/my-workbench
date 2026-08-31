ab5f157fa27f5b3e4f65d347845eedaaaa694ea9
when23 <229069994@qq.com>
2026-08-31 10:44:22 +0800

    Deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 网络优先策略，确保刷新能看到最新版本
-const CACHE = 'daily-workspace-v35';
+const CACHE = 'daily-workspace-v36';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
